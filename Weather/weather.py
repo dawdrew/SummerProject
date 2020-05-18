5b1eef796734a8ea77e8770bf23464ac24d7c14f
Andrew <dawdrew@gmail.com>
2020-05-18 13:24:07 -0400

    weather catch up

--- Weather/weather.py
+++ Weather/weather.py
@@ -3,10 +3,13 @@ import json
 # http://api.openweathermap.org/data/2.5/weather?q=dresher&appid=d6d8ad8bf09830790c7682b34d393742
 key = "d6d8ad8bf09830790c7682b34d393742"
 location = "dresher"
-req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid='+key)
+url = 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid='+key
+req = requests.get(url)
 print(req.status_code)
 
-
+# reqxml = requests.get(url+"&mode=xml")
+data = req.json()
+print(data)
 
 
 
