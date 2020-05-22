a442f47246291e0bfd1684e28102959bf5796066
Andrew <dawdrew@gmail.com>
2020-05-21 21:19:37 -0400

    started kiosk project

--- pykiosk/__init__.py
+++ pykiosk/__init__.py
@@ -7,8 +7,8 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='dev', # this app is NOT SECURE until changed remember me!!!!!!!!!!
+        DATABASE=os.path.join(app.instance_path, 'pykiosk.sqlite'),
     )
 
     if test_config is None:
