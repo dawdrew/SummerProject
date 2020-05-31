97dd0f17b001f4b66f14606271ea91bb8298bcf0
Andrew <dawdrew@gmail.com>
2020-05-31 16:04:49 -0400

    added new values to the database and changes the HTML to agree

--- pykiosk/blog.py
+++ pykiosk/blog.py
@@ -12,31 +12,36 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, name, street_address, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
+
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        name = request.form['name']
+        street_address = request.form['street_address']
+        city = request.form['city']
+        state = request.form['state']
+        zip_code = request.form['zip_code']
+        card_number = request.form['card_number']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not name:
+            error = 'Name is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (name, street_address, city, state, zip_code, card_number, author_id)'
+                ' VALUES (?, ?, ?, ?, ?, ?, ?)',
+                (name, street_address, city, state, zip_code, card_number, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -46,7 +51,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, name, street_address, city, state, zip_code, card_number, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -67,21 +72,25 @@ def update(id):
     post = get_post(id)
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        name = request.form['name']
+        street_address = request.form['street_address']
+        city = request.form['city']
+        state = request.form['state']
+        zip_code = request.form['zip_code']
+        card_number = request.form['card_number']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not name:
+            error = 'Name is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
+                'UPDATE post SET name = ?, street_address = ?, city = ? ,state = ? ,zip_code = ? ,card_number = ?'
+                'WHERE id = ?',
+                (name, street_address, city, state, zip_code, card_number, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
