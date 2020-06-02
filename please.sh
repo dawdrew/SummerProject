2aa9b623503d55136657a14e4bedd55973eb023b
Andrew <dawdrew@gmail.com>
2020-06-02 16:09:21 -0400

    .sh update

--- please.sh
+++ please.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo run server? type 'y'
-read var1
+echo run server? type 'y';
+read var1;
 
 if [ "$var1" == "y" ]; then
-  cd /
-  cd /home/pi/Documents/python/fulgen/SummerProject || exit
-  . venv/bin/activate || exit
-  export FLASK_APP=pykiosk
-  export FLASK_ENV=development
-  flask run --host=0.0.0.0
+  cd /;
+  cd /home/pi/Documents/python/fulgen/SummerProject || exit;
+  . venv/bin/activate || exit;
+  export FLASK_APP=pykiosk;
+  export FLASK_ENV=development;
+  flask run --host=0.0.0.0;
 else
-    echo "okay"
+    echo "okay";
