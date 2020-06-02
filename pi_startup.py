ca9fb093345dffc0a797ca972c5fda804d20b1c3
Andrew <dawdrew@gmail.com>
2020-06-02 12:38:59 -0400

    file for py to start on startup

---
+++ pi_startup.py
@@ -0,0 +1,10 @@
+import os
+
+runserver = input('Run Fulgen Server??--Y/n')
+
+if runserver == 'yes' or runserver == 'y' or runserver == "Y" or runserver == "Yes":
+    os.system("cd home//pi//Documents//python//fulgen//SummerProject")
+    os.system(". venv/bin/activate")
+    os.system('export FLASK_APP=pykiosk')
+    os.system('export FLASK_ENV=development')
+    os.system('flask run --host=0.0.0.0')
